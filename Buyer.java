2d5ac8dad94587fa144dfbadd41846c4e11f801f
Luba <bart6891@yandwx.ru>
2023-10-20 10:16:26 +0300

    1

---
+++ Buyer.java
@@ -0,0 +1,8 @@
+public abstract class Buyer implements BuyerBehavior {
+    protected String name;
+    protected boolean isMakeOrderClass;
+    protected boolean isTakeOrderClass;
+
+    abstract String getName();
+
+}
